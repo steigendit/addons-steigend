83cd3b050f6e7318e86a16328dda542590df7971
steigend <git@steigendit.com>
2019-06-26 16:08:22 +0530

    renamed account parent module to provide through odoo app store for free

--- account_parent_v9/__init__.py
+++ account_parent_v9/__init__.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    ODOO, Open Source Management Solution
+#    OpenERP, Open Source Management Solution
 #    Copyright (C) 2016 - Today Steigend IT Solutions (Omal Bastin)
 #    For more details, check COPYRIGHT and LICENSE files
 #
 ##############################################################################
-
-
-from openerp import api, SUPERUSER_ID
 import models
 import wizard
-
-def load_translations(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    chart_template = env.ref('l10n_ao.l10n_ao_chart_template')
-    chart_template.process_coa_translations()
